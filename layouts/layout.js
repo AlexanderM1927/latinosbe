581ee4241642226756c54205fda062d8063324ac
AlexanderM1927 <jhon71937@gmail.com>
2022-02-17 16:16:28 -0500

    Layout creation.

---
+++ layouts/layout.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Navbar from '../sections/Navbar'
+
+const Layout = ({ children }) => {
+    const linkList = [
+        {
+            title: 'Home',
+            url: '/'
+        },
+        {
+            title: 'Jobs',
+            url: '/jobs'
+        },
+        {
+            title: 'Services',
+            url: '/services'
+        },
+        {
+            title: 'Advertising',
+            url: '/advertising'
+        },
+        {
+            title: 'Hobbies',
+            url: '/hobbies'
+        }
+    ]
+    return (
+        <div>
+            <Navbar linkList={linkList} />
+            {children}
+            <footer>This is the footer</footer>
+        </div>
+    )
+}
+
+
+export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,11 @@
 import '../scss/global.scss'
-import Navbar from '../sections/Navbar'
-import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Layout from '../layouts/Layout'
 
 export default function MyApp({ Component, pageProps }) {
-    const linkList = [
-        {
-            title: 'Home',
-            url: '/'
-        },
-        {
-            title: 'Jobs',
-            url: '/jobs'
-        },
-        {
-            title: 'Services',
-            url: '/services'
-        },
-        {
-            title: 'Advertising',
-            url: '/advertising'
-        },
-        {
-            title: 'Hobbies',
-            url: '/hobbies'
-        }
-    ]
     return (
-        <div>
-            <Navbar linkList={linkList} />
+        <Layout>
             <Component {...pageProps} />
-        </div>
+        </Layout>
     )
 }
